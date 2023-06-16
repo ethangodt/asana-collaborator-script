c5281d74310ff6a78fd006fe0902dd7f204d3372
Ethan Godt <ethan@ethangodt.com>
2023-06-15 19:23:09 -0500

    working solution

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
-const axios = require('axios');
-const querystring = require('querystring');
+const axios = require("axios");
 
-exports.handler = async function(event) {
+exports.handler = async function (event) {
   console.log("Webhook received: ", event);
 
-  // If this is the handshake to establish the webhook
-  if (event.headers['X-Hook-Secret']) {
-    console.log("Received X-Hook-Secret: ", event.headers['X-Hook-Secret']);
+  // This is the handshake to establish the webhook. It's what's called in response
+  // to initiating a webhook with:
+  //
+  //   curl -H "Authorization: Bearer your-asana-access-token" \
+  //      -H "Content-Type: application/json" \
+  //      -X POST "https://app.asana.com/api/1.0/webhooks" \
+  //      -d '{
+  //            "data": {
+  //              "resource": "your-project-id-or-task-id",
+  //              "target": "https://your-public-url.com/asana-webhook-endpoint"
+  //            }
+  //          }'
+  //
+  if (event.headers["x-hook-secret"]) {
+    console.log("Received x-hook-secret: ", event.headers["x-hook-secret"]);
     return {
       statusCode: 200,
-      headers: { 'X-Hook-Secret': event.headers['X-Hook-Secret'] },
-      body: null
+      headers: { "x-hook-secret": event.headers["x-hook-secret"] },
+      body: null,
     };
   }
 
-  // Get your Asana access token and user gid from the environment variables
   const asanaAccessToken = process.env.ASANA_ACCESS_TOKEN;
-  const userId = process.env.USER_GID;
+  const userIds = process.env.ASANA_USER_GIDS.split(",");
 
   const config = {
     headers: {
-      'Authorization': `Bearer ${asanaAccessToken}`,
-      'Content-Type': 'application/json'
-    }
+      Authorization: `Bearer ${asanaAccessToken}`,
+      "Content-Type": "application/json",
+    },
   };
 
-  const eventBody = JSON.parse(event.body);
-
-  for (let event of eventBody.events) {
-    // Event for new task or subtask creation
-    if (event.resource_subtype === "task" || event.resource_subtype === "subtask" && event.action === "added") {
+  const payload = JSON.parse(event.body);
+  for (let event of payload.events) {
+    if (
+      event.resource?.resource_subtype === "default_task" &&
+      event.action === "added"
+    ) {
       const taskId = event.resource.gid;
-
-      const addCollabUrl = `https://app.asana.com/api/1.0/tasks/${taskId}/addCollaborators`;
-
-      // Make a call to Asana API to add a collaborator
       try {
         const response = await axios.post(
-          addCollabUrl,
-          querystring.stringify({ collaborators: [userId] }),
+          `https://app.asana.com/api/1.0/tasks/${taskId}/addFollowers`,
+          {
+            data: {
+              followers: userIds,
+            },
+          },
           config
         );
         console.log("Added collaborator to task: ", response.data);
